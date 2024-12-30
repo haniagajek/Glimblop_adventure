bdd4d44eb896dee7f4e0781bafe27515891a92f7
haniagajek <97366087+haniagajek@users.noreply.github.com>
2024-12-30 16:15:40 +0100

    food can escape the matrix

--- scenes/SpawnTimer.gd
+++ scenes/SpawnTimer.gd
@@ -5,6 +5,6 @@ extends Timer
 func _on_timeout():
 	randomize()
 	var fish_instance = smallFish.instantiate() # W Godocie 4.x używamy `instantiate` do tworzenia instancji scen.
-	fish_instance.position = Vector2(randf_range(10, 990), randf_range(10, 590)) # Zastępujemy `randf_range` nową funkcją `rand_range`.
+	fish_instance.position = Vector2(randf_range(-1600, 3000), randf_range(-800, 1500)) # Zastępujemy `randf_range` nową funkcją `rand_range`.
 	get_parent().add_child(fish_instance) # Dodajemy instancję ryby do nadrzędnego węzła.
 	wait_time = randf_range(0.5, 2.0) # Poprawiamy wartość `wait_time` na losową liczbę w przedziale.
