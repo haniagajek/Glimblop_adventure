85769a6d83f5cb7bed238fe8362b4655bd76870f
haniagajek <97366087+haniagajek@users.noreply.github.com>
2025-01-06 14:40:55 +0100

    new food meter

---
+++ scenes/FoodMeter.gd
@@ -0,0 +1,10 @@
+extends TextureProgressBar
+
+func _ready():
+	# Set the initial value of the progress bar
+	value = GameController.total_points
+
+func _process(delta):
+	# Continuously check for changes in total_points and update the progress bar
+	if value != GameController.total_points:
+		value = GameController.total_points
