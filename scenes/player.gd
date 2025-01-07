2e9a5f4414f46e538f6be083bb1e718d56699e99
haniagajek <97366087+haniagajek@users.noreply.github.com>
2025-01-07 21:50:24 +0100

    add better space for function

--- scenes/player.gd
+++ scenes/player.gd
@@ -3,8 +3,11 @@ class_name Player extends CharacterBody2D
 var direction: Vector2 = Vector2.ZERO
 @export var speed: int = 400
 
+
 func _process(delta):
 	direction =  Input.get_vector("left", "right", "up", "down")
+	if GameController.total_points > 5:
+		$glimblop.texture = preload("res://sprites/glimblop-2.png")
 
 func _physics_process(delta):
 	velocity = direction * speed
