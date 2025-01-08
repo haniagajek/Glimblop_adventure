3c31a2d639b24b55c740c5e61dc168bef102a232
haniagajek <97366087+haniagajek@users.noreply.github.com>
2025-01-08 23:06:03 +0100

    add music and button in progress

--- scenes/PointsUI.gd
+++ scenes/PointsUI.gd
@@ -3,8 +3,21 @@ extends Control
 
 @onready var label =$Panel/Label
 
+var musicPlay = 0
+
 func _ready():
 	EventController.connect("points_collected", on_event_points_collected)
 
 func on_event_points_collected(value : int) -> void:
 	label.text = str(value)
+
+
+
+func _on_music_pressed():
+	if musicPlay == 1:
+		$Panel/music/AudioStreamPlayer.stop()
+		musicPlay = 0
+	elif musicPlay == 0:
+		$Panel/music/AudioStreamPlayer.play()
+		musicPlay = 1
+	
