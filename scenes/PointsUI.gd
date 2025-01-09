9cabc0d4cea8f8996086c6c87cec5cb1c682c2a1
haniagajek <97366087+haniagajek@users.noreply.github.com>
2025-01-09 21:08:11 +0100

    add working music button

--- scenes/PointsUI.gd
+++ scenes/PointsUI.gd
@@ -3,7 +3,7 @@ extends Control
 
 @onready var label =$Panel/Label
 
-var musicPlay = 0
+
 
 func _ready():
 	EventController.connect("points_collected", on_event_points_collected)
@@ -14,10 +14,9 @@ func on_event_points_collected(value : int) -> void:
 
 
 func _on_music_pressed():
-	if musicPlay == 1:
-		$Panel/music/AudioStreamPlayer.stop()
-		musicPlay = 0
-	elif musicPlay == 0:
-		$Panel/music/AudioStreamPlayer.play()
-		musicPlay = 1
-	
+	var musicBg = $Panel/music/AudioStreamPlayer
+	if musicBg.stream_paused == false:
+		musicBg.stream_paused = true
+	else:
+		musicBg.stream_paused = false
+
